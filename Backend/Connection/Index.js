806d6b189a63503df872a73436a07090a97c2627
SAMEERSHARMA123 <jangidsameer20@gmail.com>
2025-08-25 17:52:56 +0530

    Update Index.js

--- Backend/Connection/Index.js
+++ Backend/Connection/Index.js
@@ -8,7 +8,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://backen-1-56c5.onrender.com
+             ');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
